d22872ee527a6b33bc4c221722cbf02bb6d4a8f9
valeria <valeriadanila99@gmail.com>
2021-03-19 15:33:37 +0200

    JS Exercise 2

---
+++ exercise2.js
@@ -0,0 +1,8 @@
+let myAge = 21 ; //My age 
+let earlyYears = 2 ; // 2 Years of a dogs life
+earlyYears = earlyYears * 10.5 ; 
+let laterYears = myAge - 2 ; /* I found out laterYears by subtracting 2 from  my actual age. */
+laterYears *= 4 ; //I calculated the number of dog years acounted for laterYears
+let myAgeInDogYears = earlyYears + laterYears ; /*I calculated my age in dogs years by adding earlyYears and laterYears */
+let myName = 'Valeria'.toLowerCase() ; //My name will be written in lower case. 
+console.log (`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`) ;  
